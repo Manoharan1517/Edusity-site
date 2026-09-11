c41331856483025e289a0ae05e591658b16e922d
Manoharan <146829898+Manoharan1517@users.noreply.github.com>
2026-09-11 14:48:50 +0530

    Fix navbar CSS import

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './navbar.css';
+import './Navbar.css';
 import logo from '../../assets/logo.png';
 import menu_icon from '../../assets/menu-icon.png';
 import { Link } from 'react-scroll';
